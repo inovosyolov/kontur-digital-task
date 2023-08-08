63323bc9242f913a00c7e00598205e9f2e6e7cb0
Ivan <inovosyolov2000@gmail.com>
2023-08-09 00:43:40 +0700

    Include modal window

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,64 @@
+let modal = document.querySelector('.modal');
+let modalBox = document.querySelector('.modal__box');
+let bannerBtn = document.querySelectorAll('.banner__btn');
+let modalCloseBtn = document.querySelector('.modal__close-btn');
+
+let selector = document.querySelector("input[type='tel']");
+let im = new Inputmask("+7 999 - 999 - 99 - 99");
+
+bannerBtn.forEach(function (el) {
+  el.addEventListener('click', function () {
+    modal.classList.toggle('modal_active');
+  })
+});
+
+modalCloseBtn.addEventListener('click', function () {
+  modal.classList.remove('modal_active');
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === "Escape") {
+    modal.classList.remove('modal_active');
+  }
+});
+
+modalBox.addEventListener('click', event => {
+  event._isClickWithInModal = true;
+});
+
+modal.addEventListener('click', event => {
+  if (event._isClickWithInModal) return;
+  event.currentTarget.classList.remove('modal_active');
+})
+
+im.mask(selector);
+
+new JustValidate('.modal__form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 30,
+    },
+    tel: {
+      required: true,
+      function: (name, value) => {
+        const phone = selector.inputmask.unmaskedvalue()
+        return Number(phone) && phone.length === 10
+      }
+    },
+    mail: {
+      required: true,
+      email: true
+    },
+  },
+  messages: {
+    name: 'Вы не ввели имя',
+    tel: 'Вы не ввели телефон',
+    mail: 'Вы не ввели e-mail'
+  },
+});
+
 const swiper = new Swiper('.banner__swiper', {
   resizeObserver: true,
   updateOnWindowResize: true,
